e0408fa2f67ed5551c9774803ded87c593ea671f
Roy Matthew Ramos <roymr@uci.edu>
2020-02-01 11:34:27 -0800

    threading server works

--- Client.py
+++ Client.py
@@ -1,24 +1,44 @@
 import socket 
+import sys 
 
 HOST = '127.0.0.1'
 PORT = 5050
 
 def client_prog():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
+        try:
+            s.connect((HOST, PORT))
+        except: 
+            print("Connection Error")
+            sys.exit()
         
-        data = s.recv(1024)
-        print(data.decode())
+        server_msg = s.recv(1024)
+        print(server_msg.decode())
 
+        print("Please enter 'quit' when you are done.")
         msg = input("-> ")
-
-        while msg.lower().strip() != 'bye':
-            s.sendall(msg.encode())
-            
+        
+        while msg.lower().strip() != 'quit':
+            s.sendall(msg.encode("utf8"))
+            # if s.recv(5120).decode("utf8") == "-":
+                 # pass # null operation
             msg = input("-> ")
 
         print("Connection about to be halted.")
-        s.close()
+        s.send(b'--quit--')
+
+        # data = s.recv(1024)
+        # print(data.decode())
+
+        # msg = input("-> ")
+
+        # while msg.lower().strip() != 'bye':
+        #     s.sendall(msg.encode())
+            
+        #     msg = input("-> ")
+
+        # print("Connection about to be halted.")
+        # s.close()
 
 
 if __name__ == '__main__':
--- Server.py
+++ Server.py
@@ -1,27 +1,90 @@
 import socket 
+import sys
+import traceback
+import threading
 
 HOST = '127.0.0.1'
 PORT = 5050
 
 def server_prog():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen(5)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    print("Socket creation is finished")
+
+    try:
+        s.bind((HOST,PORT))
+    except:
+        print("Binding failed. Error: " + str(sys.exc_info()))
+        sys.exit()
+
+    s.listen(6)
+    print("Socket now listening")
+
+    while True: 
+        conn, addr = s.accept()
+        ip, port = str(addr[0]), str(addr[1])
+        # print("Connected with " + ip + ":" + port)
+        conn.sendall("SERVER >> Leave a Congratulatory message here. Your message will be posted.".encode())          
+        print("Message Server sent to new Client")
+
+        threading.Thread(target=clientThread, args=(conn, ip, port)).start()
+
+    
+    # try:
+    #     print("Establishing thread")
+    #     Thread(target=client_thread, args=(connection, ip, port)).start()
+    # except: 
+    #     print("Thread not established.")
+    #     traceback.print_exc()  
+
+    # s.close() 
+
+def clientThread(connection, ip, port, max_buffer_size = 5120):
+   print("Entered clientThread")
+   is_active = True
+   while is_active:
+      client_input = receive_input(connection, max_buffer_size)
+      if "--QUIT--" in client_input:
+         print("Client is requesting to quit")
+         connection.close()
+         print("Connection " + ip + ":" + port + " closed")
+         is_active = False
+      else:
+         print("Processed result: {}".format(client_input))
+         connection.sendall("-".encode("utf8"))
+
+def receive_input(connection, max_buffer_size):
+   client_input = connection.recv(max_buffer_size)
+   client_input_size = sys.getsizeof(client_input)
+   if client_input_size > max_buffer_size:
+      print("The input size is greater than expected {}".format(client_input_size))
+   decoded_input = client_input.decode("utf8").rstrip()
+   result = process_input(decoded_input)
+   return result
+
+def process_input(input_str):
+   print("Processing the input received from client")
+   return "Hello " + str(input_str).upper()
+        
+# def server_prog():
+#     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#         s.bind((HOST, PORT))
+#         s.listen(5)
 
 
-        while True:
-            conn, addr = s.accept()
-            with conn: 
-                print('Connection established by ', addr)
-                conn.sendall("SERVER >> Leave a Congratulatory message here. Your message will be posted.".encode())          
+#         while True:
+#             conn, addr = s.accept()
+#             with conn: 
+#                 print('Connection established by ', addr)
+#                 conn.sendall("SERVER >> Leave a Congratulatory message here. Your message will be posted.".encode())          
 
-                while True: 
-                    data = conn.recv(1024)
-                    print(data.decode())
-                    if not data: 
-                        break
-            if data.decode() == 'q':
-                break
+#                 while True: 
+#                     data = conn.recv(1024)
+#                     print(data.decode())
+#                     if not data: 
+#                         break
+#             if data.decode() == 'q':
+#                 break
 
         
     
